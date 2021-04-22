a081eb75d51ed6dbc2c91e4a3c2c6f8825d62968
Stephen Cobbe <stephen@opal.dev>
2021-04-22 14:02:57 -0700

    v0.7.2

--- Formula/opal-security.rb
+++ Formula/opal-security.rb
@@ -3,8 +3,8 @@ require "language/node"
 class OpalSecurity < Formula
   desc "Opal allows you to centrally manage access to all of your sensitive systems"
   homepage "https://github.com/cli/opal"
-  url "https://registry.npmjs.org/opal-security/-/opal-security-0.7.1.tgz"
-  sha256 "78ff806d0db804a6055d1379da9918a9dcf482e8e41d04be0f2fdad6c596ee9c"
+  url "https://registry.npmjs.org/opal-security/-/opal-security-0.7.2.tgz"
+  sha256 "79b1346721cae8365f4999023f476bf31e167b4908ed5ff346acbb764b503e9a"
   license "MIT"
 
   livecheck do
