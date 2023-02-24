238eab9a7f009555420dca6795bd0396e658f787
Josh Chen <josh@opal.dev>
2023-02-24 15:00:52 -0500

    v2.0.20

--- Formula/opal-security.rb
+++ Formula/opal-security.rb
@@ -3,8 +3,8 @@ require "language/node"
 class OpalSecurity < Formula
   desc "Opal allows you to centrally manage access to all of your sensitive systems"
   homepage "https://github.com/opalsecurity/opal-cli/"
-  url "https://registry.npmjs.org/opal-security/-/opal-security-2.0.19.tgz"
-  sha256 "a897464eaa4bb7133e54e1842500e2e12438d81fcbe77aa3e1cc8689e101f56c"
+  url "https://registry.npmjs.org/opal-security/-/opal-security-2.0.20.tgz"
+  sha256 "23644067644d98cffd9dd17672e2ad3bc4a1c9c01976c3675f165b66b29c04b8"
   license "MIT"
 
   livecheck do
