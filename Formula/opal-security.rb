09456c58744ea0b40eb4778033d2dfa97650528e
Anna Hagiwara <anna@opal.dev>
2025-09-30 11:28:33 -0700

    Update homebrew to v.3.2.3 (#26)

--- Formula/opal-security.rb
+++ Formula/opal-security.rb
@@ -3,8 +3,8 @@ require "language/node"
 class OpalSecurity < Formula
   desc "Opal allows you to centrally manage access to all of your sensitive systems"
   homepage "https://github.com/opalsecurity/opal-cli/"
-  url "https://registry.npmjs.org/opal-security/-/opal-security-3.2.2.tgz"
-  sha256 "2e5aade1e5357450ee423fe65266013174392361abf3d0daca4a29f0d2df5e99"
+  url "https://registry.npmjs.org/opal-security/-/opal-security-3.2.3.tgz"
+  sha256 "b0229b46185449e12eccb416f26e5384f5bbb34ff3cbcd261053d3a65ff2de76"
   license "MIT"
 
   livecheck do
